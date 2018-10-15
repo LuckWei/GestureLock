4be0ccaa6507ce43be74a24ae7ff555ed9e0b90f
zhiwei <zhiwei@htech.vip>
2018-10-15 16:32:14 +0800

    Callback修改为Lambda表达式_r2

--- sample/src/main/java/catt.sample/MainActivity.kt
+++ sample/src/main/java/catt.sample/MainActivity.kt
@@ -2,11 +2,28 @@ package catt.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mGestureLockView1.setOnUnlockEventListener({
+
+        }, {
+
+        })
+        mGestureLockView2.setOnUnlockEventListener({
+
+        }, {
+
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
     }
 }
